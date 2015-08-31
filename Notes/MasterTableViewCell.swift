724772d830f9afbcad5577f0864d7331c5487212
Ryan Yue <yue.ryan123@gmail.com>
2015-08-30 21:30:43 -0700

    first commit

---
+++ Notes/MasterTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MasterTableViewCell.swift
+//  Notes
+//
+//  Created by Ryan Yue on 8/24/15.
+//  Copyright (c) 2015 Ryan Yue. All rights reserved.
+//
+
+import UIKit
+
+class MasterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
